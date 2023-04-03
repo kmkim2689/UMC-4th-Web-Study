8ec6413fd13f2bc05e73f39fe624325c8173cda0
Kim Gimun <kmkim7575@gmail.com>
2023-04-04 00:58:37 +0900

    [ADD] 메인 화면 검색기능 및 배너 자동 슬라이드 추ㄱ

--- Week01_KM/index.js
+++ Week01_KM/index.js
@@ -29,3 +29,17 @@ buttonRight.addEventListener('click', () => {
   currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
   innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
 });
+
+function getResult() {
+  let input = document.getElementById("search_content").value;
+  window.location.href = "https://www.inflearn.com/courses?s="+input
+
+}
+
+function changeImage() {
+  currentIndex = (currentIndex + 1) % 3;
+  currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
+  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+}
+
+setInterval(changeImage, 3000);
